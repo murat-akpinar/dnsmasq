925ca65c450b6aa3e1a39a883b87005b902cb482
Murat Akpınar <112278587+murat-akpinar@users.noreply.github.com>
2024-02-11 04:19:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,18 +13,21 @@ echo "53 portu UFW üzerinden açılıyor..."
 sudo ufw allow 53/tcp
 sudo ufw allow 53/udp
 
-# /etc/resolv.conf dosyasını kaldır ve sembolik link oluştur
-echo "/etc/resolv.conf dosyası yeniden yapılandırılıyor..."
-sudo rm /etc/resolv.conf
-echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
-
 # systemd-resolved servisini durdur ve devre dışı bırak
 echo "systemd-resolved servisi durduruluyor ve devre dışı bırakılıyor..."
 sudo systemctl stop systemd-resolved
 sudo systemctl disable systemd-resolved
 
+# /etc/resolv.conf dosyasını kaldır ve sembolik link oluştur
+echo "/etc/resolv.conf dosyası yeniden yapılandırılıyor..."
+sudo rm /etc/resolv.conf
+echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
+
 # dnsmasq servisini yeniden başlat
 sudo systemctl restart dnsmasq
+sudo systemctl start dnsmasq
+sudo systemctl enable --now dnsmasq
+sudo systemctl status dnsmasq
 
 # Kullanıcıya son adımlar hakkında bilgi verme
 echo "Kurulum tamamlandı."
